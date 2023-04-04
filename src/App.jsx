2c1616680cd3cef52010431455a917bf76b76ad3
andimahathir <enlandiy@ymail.com>
2023-04-04 15:25:24 +0700

    convert count app to todo list app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [activity, setActivity] = useState("");
+    const [todos, setTodos] = useState([]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    function addTodo(event) {
+        event.preventDefault();
+
+        setTodos([
+            ...todos,
+            { id: Date.now(), title: activity, completed: false },
+        ]);
+
+        setActivity("");
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://reactjs.org" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>ToDo What ToDo</h1>
+            <div className="card">
+                <form onSubmit={addTodo}>
+                    <input
+                        type="text"
+                        placeholder="What you gonna do?"
+                        value={activity}
+                        onChange={(event) => setActivity(event.target.value)}
+                    />
+                    <button type="submit">Add Task</button>
+                </form>
+            </div>
+            <div className="card">
+                <h2>Task(s) ToDo</h2>
+                <ul className="no-bullets">
+                    {todos.map((todo, index) => (
+                        <li key={index}>{todo.title}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
